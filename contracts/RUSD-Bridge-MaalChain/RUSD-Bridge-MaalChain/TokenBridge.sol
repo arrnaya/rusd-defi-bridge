de4eb3cf880d12a53e62a8f804e6c7dc1c72c152
Arrnaya <39946946+arrnaya@users.noreply.github.com>
2025-05-14 19:08:05 +0530

    maalv2 to bsc testnet updated
    Solidity version fixed to 0.8.17 for MaalChain

--- contracts/RUSD-Bridge-MaalChain/RUSD-Bridge-MaalChain/TokenBridge.sol
+++ contracts/RUSD-Bridge-MaalChain/RUSD-Bridge-MaalChain/TokenBridge.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.20;
+pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
+import "./IERC20.sol";
+import "./SafeERC20.sol";
+import "./ReentrancyGuard.sol";
+import "./AccessControl.sol";
+import "./Pausable.sol";
 
 interface IERC20Detailed is IERC20 {
     function name() external view returns (string memory);
--- lib/constants.ts
+++ lib/constants.ts
@@ -2,20 +2,20 @@ import { Chains, Token } from '@/types';
 import { Address } from 'viem';
 
 export const CHAINS: Chains[] = [
-    {
-        id: 7863,
-        name: 'MaalChain Testnet',
-        logo: '/chains/maalchain.avif',
-        network: 'maalchain-testnet',
-        nativeCurrency: { name: 'Maal', symbol: 'MAAL', decimals: 18 },
-        rpcUrls: {
-            default: { http: ['https://node-testnet.maalscan.io'] },
-            public: { http: ['https://node-testnet.maalscan.io'] },
-        },
-        blockExplorers: {
-            default: { name: 'MaalScan', url: 'https://new-testnet.maalscan.io/' },
-        },
-    },
+    // {
+    //     id: 7863,
+    //     name: 'MaalChain Testnet',
+    //     logo: '/chains/maalchain.avif',
+    //     network: 'maalchain-testnet',
+    //     nativeCurrency: { name: 'Maal', symbol: 'MAAL', decimals: 18 },
+    //     rpcUrls: {
+    //         default: { http: ['https://node-testnet.maalscan.io'] },
+    //         public: { http: ['https://node-testnet.maalscan.io'] },
+    //     },
+    //     blockExplorers: {
+    //         default: { name: 'MaalScan', url: 'https://new-testnet.maalscan.io/' },
+    //     },
+    // },
     {
         id: 97,
         name: 'BSC Testnet',
@@ -30,20 +30,20 @@ export const CHAINS: Chains[] = [
             default: { name: 'BscScan', url: 'https://testnet.bscscan.com' },
         },
     },
-    // {
-    //     id: 7862,
-    //     name: 'MaalChain V2',
-    //     logo: '/chains/maalchain.avif',
-    //     network: 'maalchain-v2',
-    //     nativeCurrency: { name: 'Maal', symbol: 'MAAL', decimals: 18 },
-    //     rpcUrls: {
-    //         default: { http: ['https://node1-mainnet-new.maalscan.io'] },
-    //         public: { http: ['https://node1-mainnet-new.maalscan.io'] },
-    //     },
-    //     blockExplorers: {
-    //         default: { name: 'MaalScan', url: 'https://v2.maalscan.io/' },
-    //     },
-    // },
+    {
+        id: 7862,
+        name: 'MaalChain V2',
+        logo: '/chains/maalchain.avif',
+        network: 'maalchain-v2',
+        nativeCurrency: { name: 'Maal', symbol: 'MAAL', decimals: 18 },
+        rpcUrls: {
+            default: { http: ['https://node1-mainnet-new.maalscan.io'] },
+            public: { http: ['https://node1-mainnet-new.maalscan.io'] },
+        },
+        blockExplorers: {
+            default: { name: 'MaalScan', url: 'https://v2.maalscan.io/' },
+        },
+    },
     // {
     //     id: 56,
     //     name: 'BSC Mainnet',
@@ -58,20 +58,20 @@ export const CHAINS: Chains[] = [
     //         default: { name: 'BscScan', url: 'https://bscscan.com' },
     //     },
     // },
-    {
-        id: 17000,
-        name: 'Holesky Testnet',
-        logo: '/chains/empty-token.webp',
-        network: 'holesky',
-        nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
-        rpcUrls: {
-            default: { http: ['https://holesky.gateway.tenderly.co/'] },
-            public: { http: ['https://holesky.gateway.tenderly.co/'] },
-        },
-        blockExplorers: {
-            default: { name: 'Holesky-scan', url: 'https://holesky.etherscan.io/' },
-        },
-    }
+    // {
+    //     id: 17000,
+    //     name: 'Holesky Testnet',
+    //     logo: '/chains/empty-token.webp',
+    //     network: 'holesky',
+    //     nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+    //     rpcUrls: {
+    //         default: { http: ['https://holesky.gateway.tenderly.co/'] },
+    //         public: { http: ['https://holesky.gateway.tenderly.co/'] },
+    //     },
+    //     blockExplorers: {
+    //         default: { name: 'Holesky-scan', url: 'https://holesky.etherscan.io/' },
+    //     },
+    // }
 ];
 
 export const TOKENS: Token[] = [
@@ -89,27 +89,27 @@ export const TOKENS: Token[] = [
         decimals: 18,
         chainId: 7863,
     },
-    // {
-    //     address: '0x6b3b95B0c84f35c9c505fff8C2F35Ee2e502a44D' as Address,
-    //     symbol: 'RUSD',
-    //     name: 'Royal Dollar',
-    //     decimals: 18,
-    //     chainId: 56, // BSC Mainnet
-    // },
-    // {
-    //     address: '0x49561Eb00e1E2Ff7a3E2a7c9664cEAa2Ce365a10' as Address,
-    //     symbol: 'RUSD',
-    //     name: 'Royal Dollar',
-    //     decimals: 18,
-    //     chainId: 7862, // MaalChain V2
-    // },
     {
-        address: '0xaCcfdB7767ddcF5f088ec9B8d726A76b29BAB768' as Address,
-        symbol: 'USDT',
-        name: 'USD Tether',
+        address: '0x6b3b95B0c84f35c9c505fff8C2F35Ee2e502a44D' as Address,
+        symbol: 'RUSD',
+        name: 'Royal Dollar',
         decimals: 18,
-        chainId: 17000, // Holesky Testnet
+        chainId: 56, // BSC Mainnet
     },
+    {
+        address: '0x49561Eb00e1E2Ff7a3E2a7c9664cEAa2Ce365a10' as Address,
+        symbol: 'RUSD',
+        name: 'Royal Dollar',
+        decimals: 18,
+        chainId: 7862, // MaalChain V2
+    }
+    // {
+    //     address: '0xaCcfdB7767ddcF5f088ec9B8d726A76b29BAB768' as Address,
+    //     symbol: 'USDT',
+    //     name: 'USD Tether',
+    //     decimals: 18,
+    //     chainId: 17000, // Holesky Testnet
+    // },
     // {
     //     address: '0x49561Eb00e1E2Ff7a3E2a7c9664cEAa2Ce365a10' as Address,
     //     symbol: 'RUSD',
@@ -122,18 +122,18 @@ export const TOKENS: Token[] = [
 export const TOKEN_BRIDGE_ADDRESSES: Record<number, Address> = {
     // 56: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_BSC_MAIN! as Address,
     97: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_BSC! as Address,
-    17000: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_HOLESKY! as Address,
-    // 7863: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL! as Address,
-    // 7862: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL_MAIN! as Address,
+    // 17000: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_HOLESKY! as Address,
+    7863: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL! as Address,
+    7862: process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL_MAIN! as Address,
 };
 
 // Validate environment variables
 if (
     !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_BSC ||
-    !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_HOLESKY
+    // !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_HOLESKY ||
     // !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_BSC_MAIN ||
-    // !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL
-    // !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL_MAIN
+    !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL ||
+    !process.env.NEXT_PUBLIC_TOKEN_BRIDGE_ADDRESS_MAAL_MAIN
 ) {
     console.warn('Missing TokenBridge contract addresses in environment variables');
 }
